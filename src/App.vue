b7d42607c6992a1ed57103d60c750e796ba6293b
emamoldo <moldovanemanuel92.em@gmail.com>
2024-04-03 14:25:25 +0200

    Add Js Script for the moviesList

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <script>
-
+import axios from 'axios';
+export default {
+  name: 'App',
+  data() {
+    return {
+      moviesList: []
+    }
+  },
+  mounted() {
+    axios
+      .get('https://api.themoviedb.org/3/search/movie?api_key=1167e581ec02fa604200a947a803071e&query=')
+      .then(resp => {
+        console.log(resp.data.results);
+        this.moviesList = resp.data.results;
+      })
+  }
+}
 </script>
+<!-- remember to install axios -->
+
 
 <template>
-  <div>
-
-  </div>
 
 </template>
+
+
 
 <style></style>
