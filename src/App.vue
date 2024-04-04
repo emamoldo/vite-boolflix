45d59228ce136ded5b385af48bcdc0e04982016a
emamoldo <moldovanemanuel92.em@gmail.com>
2024-04-04 17:41:33 +0200

    Add The Number Ceil

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,11 @@ export default {
       moviesList: [],
       searchText: '',
       tvSeries: [],
-      languageFlag: [],
     }
   },
 
   methods: {
+
     // For the Movies
     search() {
       this.searchMovies()
@@ -21,12 +21,10 @@ export default {
       axios
         .get(url)
         .then(resp => {
-          // console.log(resp.data.results, this.moviesList);
           this.moviesList = resp.data.results;
         })
     },
     searchMovies() {
-      console.log(this.searchText);
       this.movies(`https://api.themoviedb.org/3/search/movie?api_key=1167e581ec02fa604200a947a803071e&query=${this.searchText}`)
     },
 
@@ -35,27 +33,27 @@ export default {
       axios
         .get(url)
         .then(resp => {
-          // console.log(resp.data.results, this.tvSeries);
-
-          console.log("serie:", resp.data.results)
           this.tvSeries = resp.data.results;
         })
     },
     searchSeries() {
-      console.log(this.searchText);
       this.series(`https://api.themoviedb.org/3/search/tv?api_key=1167e581ec02fa604200a947a803071e&language=&query=${this.searchText}`)
-    },
+    }
   },
-
   mounted() {
     this.movies('https://api.themoviedb.org/3/search/movie?api_key=1167e581ec02fa604200a947a803071e&query=')
     this.series('https://api.themoviedb.org/3/search/tv?api_key=1167e581ec02fa604200a947a803071e&language=&query=')
-  }
+  },
 }
 </script>
 
 
 <template>
+  <!-- FontAwesome Cdn -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
   <header>
     <div class="searchbox">
       <input type="text" v-model="searchText" @keyup="search" placeholder="Search Movie">
@@ -66,47 +64,49 @@ export default {
 
   <main>
     <section class="movies">
-      <div class="container">
-        <div class="row">
-          <h1>Movies:</h1>
-          <div v-for="movie in moviesList">
-            <div class="movies">
+      <div class="container_movie">
+        <h1>Movies:</h1>
+        <div v-for="movie in moviesList">
+          <div class="movies">
+            <div>
+              {{ movie.title }}
+              <br>
+
+              {{ movie.original_title }}
+              <br>
+
+              <img :src="`https://flagsapi.com/${movie.original_language.toUpperCase()}/flat/48.png`" alt="">
+              <br>
+
+              {{ movie.original_language }}
+              <br>
+
+              <img :src="`https://image.tmdb.org/t/p/w185/${movie.poster_path}`" alt="">
+              <br>
+
+              {{ Math.ceil(movie.vote_average / 2) }}
+              <!-- <i class="fa-solid fa-star"></i> -->
+              <br>
+            </div>
+          </div>
+
+          <div class="col" v-for="serie in tvSeries">
+            <h2>TV Series:</h2>
+            <div class="series">
               <div>
-                {{ movie.title }}
+                {{ serie.original_name }}
                 <br>
 
-                {{ movie.original_title }}
+                <img :src="`https://flagsapi.com/${serie.origin_country}/flat/48.png`" alt="">
                 <br>
 
-                <img :src="`https://flagsapi.com/${movie.original_language.toUpperCase()}/flat/48.png`" alt="">
+                <img :src="`https://image.tmdb.org/t/p/w185/${serie.poster_path}`" alt="">
                 <br>
-
-                {{ movie.original_language }}
+                {{ Math.ceil(serie.vote_average / 2) }}
                 <br>
-
-                <img :src="`https://image.tmdb.org/t/p/w185/${movie.poster_path}`" alt="">
-                <br>
-
-                {{ movie.vote_average }}
                 <br>
               </div>
             </div>
-
-            <div class="col" v-for="serie in tvSeries">
-              <h2>TV Series:</h2>
-              <div class="series">
-                <div>
-                  {{ serie.original_name }}
-                  <br>
-                  {{ serie.origin_country }}
-                  <br>
-                  {{ serie.vote_average }}
-                  <br>
-                  <br>
-                </div>
-              </div>
-            </div>
-
           </div>
         </div>
       </div>
